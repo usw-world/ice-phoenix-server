847596e7de6e90aa87982c5bb00d96c314f7961f
usoock13 <usoock13@gmail.com>
2022-11-27 03:28:50 +0900

    IFSerfer prototype 0.1version is comming.

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,5 +1,5 @@
 // import { QueryCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
-import { ExecuteStatementCommand } from "@aws-sdk/client-dynamodb";
+import { BatchExecuteStatementCommand, ExecuteStatementCommand } from "@aws-sdk/client-dynamodb";
 import { ddbDocClient } from "./ddbDocument";
 
 const PARTITION_KEY = "userKey";
@@ -7,23 +7,105 @@ const SORT_KEY = "index";
 
 const tableName = "ice-phoenix-user";
 
-export const QueryUser = async () => {
+export const CheckUser = async (userKey) => {
   const params = {
-    Statement: "SELECT * FROM " + tableName + " where index>=?",
-    Parameters: [{ N: 0 }],
+    Statement: `SELECT "userKey", "index" 
+                FROM "${tableName}" 
+                WHERE "userKey" = ?
+    `,
+    Parameters: [{ S: userKey }],
   };
   try {
     const data = await ddbDocClient.send(new ExecuteStatementCommand(params));
-    for (let i = 0; i < data.Items.length; i++) {
-      console.log(
-        "Success. The query return the following data. Item " + i,
-        data.Items[i].year,
-        data.Items[i].title,
-        data.Items[i].info
-      );
-    }
-    return "Run successfully"; // For unit tests.
+    if(data.Items.length > 0)  return false;
+    else                       return true;
   } catch (err) {
     console.error(err);
+    return false;
   }
 };
+export const AddUser = async (userKey) => {
+  const params = {
+    Statement: `INSERT INTO "${tableName}"
+                VALUE {
+                  'userKey' : ?,
+                  'level' : ?,
+                  'sceneNo' : ?,
+                  'clearCount' : ?,
+                  'adaptation' : ?
+                }
+    `,
+    Parameters: [
+      { S: userKey },
+      { N: '1' },
+      { N: '0' },
+      { N: '0' },
+      { L: [{N: '0'}, {N: '0'}, {N: '0'}, {N: '0'}, {N: '0'}] },
+    ],
+  };
+  try {
+    await ddbDocClient.send(new ExecuteStatementCommand(params));
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+export const GetData = async (userKey) => {
+  const params = {
+    Statement: `SELECT "userKey", "level", "sceneNo", "clearCount", "adaptation"
+                FROM "${tableName}"
+                WHERE "userKey" = ?
+    `,
+    Parameters: [{ S: userKey }]
+  }
+  try {
+    const data = await ddbDocClient.send(new ExecuteStatementCommand(params));
+    if(data.Items.length > 0) {
+      let gameData = {
+        "userKey" : data.Items[0].userKey.S,
+        "level" : data.Items[0].level.N,
+        "sceneNo" : data.Items[0].sceneNo.N,
+        "clearCount" : data.Items[0].clearCount.N,
+        "adaptation" : Array.from(data.Items[0].adaptation.L, elmt => elmt.N),
+      };
+      return gameData;
+    } else {
+      console.error("not found user data with 'userKey' that is sended by user.");
+      return false;
+    }
+  } catch(err) {
+    console.log("Error In func::GetData");
+    console.log(err);
+  }
+};
+export const UpdateData = async (gameData) => {
+  const originData = await GetData(gameData.userKey);
+  if(originData.level > gameData.level
+  || originData.clearCount > gameData.clearCount) {
+    console.error("Server got the invaild data. Update process stoped.");
+    return false;
+  }
+  const params = {
+    Statements: [{
+      Statement: `UPDATE "${tableName}"
+                  SET "level"=?, "sceneNo"=?, "clearCount"=?, "adaptation"=?
+                  WHERE "userKey"=?
+      `,
+      Parameters: [
+        { N: ''+gameData.level },
+        { N: ''+gameData.sceneNo },
+        { N: ''+gameData.clearCount },
+        { L: Array.from(gameData.adaptation, elmt => { return { N: ''+elmt }; }) },
+        { S: gameData.userKey },
+      ]
+    }]
+  };
+  try {
+    let t = await ddbDocClient.send(new BatchExecuteStatementCommand(params))
+    return true;
+  } catch(err) {
+    console.error(err);
+    return false;
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,18 +1,56 @@
 import express from "express";
+import crypto from "crypto";
 const app = express();
 
-const { QueryUser } = require("./dynamodb");
+const { CheckUser, AddUser, GetData, UpdateData } = require("./dynamodb");
 
 const PORT_NUMBER = 2022;
 
-app.post('/create-user', (req, res) => {
-    
+app.use(express.urlencoded({ extended: true }));
+app.post('/create-user', async (req, res) => {
+    CreateUser(res);
 });
-app.post('/get-user', (req, res) => {
+app.post('/get-data', async (req, res) => {
+    const gameData = await GetData(req.body.userKey);
+    // console.log(gameData);
+    res.json(gameData);
+});
+app.post('/sync-data', async (req, res) => {
+    const gameData = {
+        ...JSON.parse(req.body.dataJson)
+    };
+    if(await UpdateData(gameData)) {
+        res.json({
+            result : 200
+        });
+    } else {
+        res.json({
+            result : 500
+        });
+    }
+});
+
+async function CreateUser(res) {
+    let userKey;
+    let tryCount = 0;
+    do {
+        tryCount ++;
+        userKey = crypto.randomBytes(64).toString('base64');
+    } while(!await CheckUser(userKey) || tryCount >= 10);
     res.setHeader("Content-Type", "application/json");
-    QueryUser();
-    res.json({name : "usoock"});
-});
+    if(await AddUser(userKey)) {
+        res.json({
+            result : 200,
+            userKey : userKey
+        });
+    } else {
+        console.log("Faild to put new user key. So return error-message to client.");
+        res.json({
+            result : 500,
+            message : "서버에서 예외가 발생하였습니다.\n서버 담당자에게 문의해주시길 바랍니다."
+        });
+    }
+}
 
 app.listen(PORT_NUMBER, () => {
     console.log("Express app is running on port " + PORT_NUMBER);
